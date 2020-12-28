4ab51b057196db5d705ffc2acd4740de1b97b744
E.Kalinin <shoreward@gmail.com>
2020-12-28 16:50:10 +0300

    team_member_detection

---
+++ go.mod
@@ -0,0 +1,8 @@
+module tdscripts
+
+go 1.14
+
+require (
+	github.com/tada-team/tdclient v0.5.3
+	github.com/tada-team/tdproto v1.19.2
+)
---
+++ team_member_detection/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+
+	"github.com/tada-team/tdclient"
+	"github.com/tada-team/tdclient/examples"
+	"github.com/tada-team/tdproto"
+)
+
+const UNKNOWN = "unknown"
+
+func main() {
+
+	checkedUID := tdproto.NewJID("d-df2d36bf-3bbb-49f2-9a1e-5e513b7597d7")
+
+	settings := examples.NewSettings()
+	settings.RequireToken()
+	settings.RequireTeam()
+	settings.Parse()
+
+	session, err := tdclient.NewSession(settings.Server)
+	if err != nil {
+		panic(err)
+	}
+
+	session.SetToken(settings.Token)
+	session.SetVerbose(settings.Verbose)
+
+	contacts, err := session.Contacts(settings.TeamUid)
+	for _, contact := range contacts {
+		//log.Println(contact.Jid.String(), checkedUID.String())
+		if contact.Jid.String() == checkedUID.String() {
+			log.Println("GOT IT!", contact.TeamStatus)
+			return
+		}
+	}
+	log.Println("Not found")
+}
